01110a0574aeb1c87e0578d48dcae8c48ebad92e
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-07-09 15:15:41 +0600

    update

--- dist/utils/apiResponse.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendApiResponse = void 0;
-const sendApiResponse = (res, apiResponse) => {
-    res.status(apiResponse.status).json(apiResponse);
-};
-exports.sendApiResponse = sendApiResponse;
--- dist/utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const responseHandler_1 = require("./responseHandler");
-const errorHandler = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "An unexpected error occurred";
-    (0, responseHandler_1.handleError)(res, err, statusCode, message);
-};
-exports.default = errorHandler;
